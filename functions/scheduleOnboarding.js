e40cc07aa9e4d49f4592ff79ecce9f38561170d6
Kevin <thebeast7991@gmail.com>
2025-09-30 01:39:11 -0700

    progress on scheduleOnboarding setup

--- functions/scheduleOnboarding.js
+++ functions/scheduleOnboarding.js
@@ -1,17 +1,11 @@
-async function placeOrder(functionArgs) {
-  const {model, quantity} = functionArgs;
-  console.log('GPT -> called placeOrder function');
+async function scheduleOnboarding(functionArgs) {
+  const { callerName, preferredTime } = functionArgs;
+  console.log('GPT -> called scheduleOnboarding function');
   
-  // generate a random order number that is 7 digits 
-  const orderNum = Math.floor(Math.random() * (9999999 - 1000000 + 1) + 1000000);
-
-  // check model and return the order number and price with 7.9% sales tax
-  if (model?.toLowerCase().includes('pro')) {
-    return JSON.stringify({ orderNumber: orderNum, price: Math.floor(quantity * 249 * 1.079)});
-  } else if (model?.toLowerCase().includes('max')) {
-    return JSON.stringify({ orderNumber: orderNum, price: Math.floor(quantity * 549 * 1.079) });
-  }
-  return JSON.stringify({ orderNumber: orderNum, price: Math.floor(quantity * 179 * 1.079) });
+  return JSON.stringify({ 
+    confirmedTime: '2024-07-01 10:00 AM', 
+    status: 'success',
+  });
 }
 
-module.exports = placeOrder;
+module.exports = scheduleOnboarding;
--- test/scheduleOnboarding.test.js
+++ test/scheduleOnboarding.test.js
@@ -1,8 +1,9 @@
 const scheduleOnboarding = require('../functions/scheduleOnboarding');
 
 test('Expect placeOrder to return an object with a price and order number', () => {
-  const order = JSON.parse(scheduleOnboarding({model: 'airpods pro', quantity: 10}));
+  const order = JSON.parse(scheduleOnboarding({ callerName: 'Kevin', preferredTime: 10 }));
 
-  expect(order).toHaveProperty('orderNumber');
-  expect(order).toHaveProperty('price');
+  expect(order).toHaveProperty('confirmedTime');
+  expect(order).toHaveProperty('status');
+  
 });
